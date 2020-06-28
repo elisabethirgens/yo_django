56a1de9ad8b74427041fd9cab65b8b7ec6ec7d4a
Elisabeth Irgens <hello@elisabethirgens.com>
2020-06-28 16:36:45 +0200

    Rename to match naming in the tutorial

--- polls/views.py
+++ polls/views.py
@@ -11,7 +11,7 @@ def index(request):
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/details.html', {'question': question})
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You are looking at the results of question %s."
